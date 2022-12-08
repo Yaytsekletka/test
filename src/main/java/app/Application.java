7af993c79b25166839f09197624ea858bd4e43cb
Alexander Ulyanov <94780419+Yaytsekletka@users.noreply.github.com>
2022-12-08 12:31:52 +0300

    in progress

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,27 @@
 package app;
 
+import io.github.humbleui.jwm.App;
 import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.Window;
 
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
+    // окно приложения
+    private final Window window;
+
+    // конструктор приложения
+    public Application() {
+        // создаём окно
+        window = App.makeWindow();
+        // задаём обработчиком событий текущий объект
+        window.setEventListener(this);
+        // делаем окно видимым
+        window.setVisible(true);
+    }
+
+    // обработчик событий
     @Override
     public void accept(Event event) {
-
     }
 }
